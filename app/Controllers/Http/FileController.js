7ed0b9f1a8a354bbde78e1adcfb2feda9fbeb18b
Jardel Kuhn - SAMSUNG <jardelkuhn@gmail.com>
2019-11-22 22:13:22 -0300

    first commit

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const File = use("App/Models/File");
+const Helpers = use("Helpers");
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  async show({ params, response }) {
+    const file = await File.findOrFail(params.id);
+    console.log(params);
+
+    return response.download(Helpers.tmpPath(`uploads/${file.file}`));
+  }
+
+  async store({ request, response }) {
+    try {
+      if (!request.file("file")) return;
+
+      const upload = request.file("file", { size: "2mb" });
+
+      const fileName = `${Date.now()}.${upload.subtype}`;
+      await upload.move(Helpers.tmpPath("uploads"), { name: fileName });
+
+      if (!upload.move()) {
+        throw upload.move();
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      });
+
+      return file;
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: "Erro no upload de arquivo" } });
+    }
+  }
+}
+
+module.exports = FileController;
